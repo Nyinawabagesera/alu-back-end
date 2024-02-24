224ad770b2fd2c2de431fe892eef4b5a53cfa14a
Nyinawabagesera <j.nyinawaba@alustudent.com>
2024-02-24 19:24:46 +0000

    file

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,31 +1,36 @@
 #!/usr/bin/python3
-"""Script to fetch TODOs for a user from API"""
+"""Script to get todos for a user from API"""
 
 import requests
 import sys
 
+
 def main():
-    """Main function to fetch and display TODOs"""
+    """main function"""
     user_id = int(sys.argv[1])
     todo_url = 'https://jsonplaceholder.typicode.com/todos'
     user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
 
     response = requests.get(todo_url)
 
-    total_tasks = 0
-    completed_tasks = []
-    for task in response.json():
-        if task['userId'] == user_id:
-            total_tasks += 1
-            if task['completed']:
-                completed_tasks.append(task['title'])
+    total_questions = 0
+    completed = []
+    for todo in response.json():
+
+        if todo['userId'] == user_id:
+            total_questions += 1
+
+            if todo['completed']:
+                completed.append(todo['title'])
 
     user_name = requests.get(user_url).json()['name']
 
-    print("Employee {} has completed tasks ({}/{}):".format(user_name,
-          len(completed_tasks), total_tasks))
-    for completed_task in completed_tasks:
-        print("\t{}".format(completed_task))
+    printer = ("Employee {} is done with tasks({}/{}):".format(user_name,
+               len(completed), total_questions))
+    print(printer)
+    for q in completed:
+        print("\t {}".format(q))
+
 
 if __name__ == '__main__':
     main()
